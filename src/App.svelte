e942277af1ddb8486dbbc690828a53bed3d4eb11
Aaron Maywood <am7274183@gmail.com>
2022-04-05 15:44:20 +0900

    Footer の実装

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,11 @@
 <script>
     import Header from './Header.svelte';
+    import Footer from './Footer.svelte';
 </script>
 
 <div class="sbdocs-wrapper">
 	<Header />
+	<Footer />
 </div>
 
 <style>
---
+++ src/Footer.svelte
@@ -0,0 +1,32 @@
+<!-- Footer -->
+<div class="tip-wrapper">
+	<span class="tip">Tip</span>Edit the Markdown in
+	<code>src/stories/Introduction.stories.mdx</code>
+</div>
+
+<style>
+.tip {
+	display: inline-block;
+	border-radius: 1em;
+	font-size: 11px;
+	line-height: 12px;
+	font-weight: 700;
+	background: #E7FDD8;
+	color: #66BF3C;
+	padding: 4px 12px;
+	margin-right: 10px;
+	vertical-align: top;
+}
+
+.tip-wrapper {
+	font-size: 13px;
+	line-height: 20px;
+	margin-top: 40px;
+	margin-bottom: 40px;
+}
+
+.tip-wrapper code {
+	font-size: 12px;
+	display: inline-block;
+}
+</style>
