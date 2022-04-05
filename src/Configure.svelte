36d3453044e2785a4a258931421b1a5251224ebf
Aaron Maywood <am7274183@gmail.com>
2022-04-05 19:14:48 +0900

    全てのListItemに値を埋めた

--- src/Configure.svelte
+++ src/Configure.svelte
@@ -6,14 +6,32 @@ import LinkItem from './LinkItem.svelte';
 <div class="link-list">
 	<LinkItem
 		href="https://storybook.js.org/docs/react/addons/addon-types"
-		src="https://raw.githubusercontent.com/storybookjs/storybook/next/examples/vue-3-cli/src/stories/assets/stackalt.svg"
+		src="https://raw.githubusercontent.com/storybookjs/storybook/next/examples/vue-3-cli/src/stories/assets/plugin.svg"
 		alt="plugin"
 		title="Presets for popular tools"
 		desc="Easy setup for TypeScript, SCSS and more."
 	/>
-	<LinkItem />
-	<LinkItem />
-	<LinkItem />
+	<LinkItem
+		href="https://storybook.js.org/docs/react/configure/webpack"
+		src="https://raw.githubusercontent.com/storybookjs/storybook/next/examples/vue-3-cli/src/stories/assets/stackalt.svg"
+		alt="Build"
+		title="Build configuration"
+		desc="How to customize webpack and Babel"
+	/>
+	<LinkItem
+		href="https://storybook.js.org/docs/react/configure/styling-and-css"
+		src="https://raw.githubusercontent.com/storybookjs/storybook/next/examples/vue-3-cli/src/stories/assets/colors.svg"
+		alt="colors"
+		title="Styling"
+		desc="How to load and configure CSS libraries"
+	/>
+	<LinkItem
+		href="https://storybook.js.org/docs/react/get-started/setup#configure-storybook-for-your-stack"
+		src="https://raw.githubusercontent.com/storybookjs/storybook/next/examples/vue-3-cli/src/stories/assets/flow.svg"
+		alt="flow"
+		title="Data"
+		desc="Providers and mocking for data libraries"
+	/>
 </div>
 
 <style>
