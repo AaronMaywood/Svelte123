2b26d207b1dd70431641ec546bacdb80ff0aadfa
Aaron Maywood <am7274183@gmail.com>
2022-04-05 17:07:16 +0900

    コンポーネント名のリファクタリング

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,11 @@
 <script>
     import Header from './Header.svelte';
-    import Footer from './Footer.svelte';
+    import Tip from './Tip.svelte';
 </script>
 
 <div class="sbdocs-wrapper">
 	<Header />
-	<Footer />
+	<Tip />
 </div>
 
 <style>
